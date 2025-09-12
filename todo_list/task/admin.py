2688e99f2af2c1b866850a5fd2eec0bfce0fcc6b
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2025-09-12 16:05:34 +0330

    chore (task): create task model

--- todo_list/task/admin.py
+++ todo_list/task/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Task)
+class UserAdmin(admin.ModelAdmin):
+    pass
--- todo_list/task/models.py
+++ todo_list/task/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(_("Task title"), max_length=50)
+    description = models.CharField(_("Task description"), max_length=255)
+    is_done = models.BooleanField(_("Is done"), default=False)
+    dead_line = models.DateTimeField(
+        _("Task dead line time"), auto_now=False, auto_now_add=False
+    )
+    is_active = models.BooleanField(_("Is valid"))
+
+    def __str__(self):
+        return self.title + " | is valid : " + self.is_done
