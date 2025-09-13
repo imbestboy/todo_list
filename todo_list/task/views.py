25911d0e0e1981df98a2af0bb1c3f71c783b0a25
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2025-09-13 16:05:10 +0330

    fix (task): delete task changed flag instead of delete record

--- todo_list/task/views.py
+++ todo_list/task/views.py
@@ -10,9 +10,10 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TaskSerializer
 
     def get_queryset(self):
-        return models.Task.objects.filter(user=self.request.user)
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
+        return models.Task.objects.filter(user=self.request.user, is_active=True)
         context.update({"user": self.request.user})
         return context
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
