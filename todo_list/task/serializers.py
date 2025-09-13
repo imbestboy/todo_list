3e83ba378205a970084d5867bf618b19830d5b19
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2025-09-13 16:06:26 +0330

    feat (user,task): add serializer for user and add user data to tasks list

--- todo_list/task/serializers.py
+++ todo_list/task/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from . import models
+from user.serializers import UserSerializer
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = models.Task
-        fields = ("title", "description", "dead_line")
+        fields = ("id", "user", "title", "description", "is_done", "dead_line")
+        read_only_fields = ("id", "user")
 
     def create(self, validated_data):
         validated_data.update({"user": self.context["user"]})
--- todo_list/user/serializers.py
+++ todo_list/user/serializers.py
@@ -4,6 +4,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name")
+
+
 class SignUpSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
